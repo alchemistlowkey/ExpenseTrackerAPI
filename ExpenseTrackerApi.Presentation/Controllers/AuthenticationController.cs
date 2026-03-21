223614920e4d355b13f23761f6a512fdcbaad857
alchemistlowkey <alchemistlowkey@gmail.com>
2026-03-21 14:51:24 +0100

    Added exception handling to Authenticate method in AuthenticationController

--- ExpenseTrackerApi.Presentation/Controllers/AuthenticationController.cs
+++ ExpenseTrackerApi.Presentation/Controllers/AuthenticationController.cs
@@ -40,14 +40,22 @@ namespace ExpenseTrackerApi.Presentation.Controllers
         [HttpPost("login")]
         public async Task<IActionResult> Authenticate([FromBody] UserForAuthenticationDto user)
         {
-            if (!await _service.AuthenticationService.ValidateUser(user))
+            try
             {
-                Log.Information($"Authentication failed for user {user.UserName}");
-                return Unauthorized();
+                if (!await _service.AuthenticationService.ValidateUser(user))
+                {
+                    Log.Information($"Authentication failed for user {user.UserName}");
+                    return Unauthorized();
+                }
+                var tokenDto = await _service.AuthenticationService.CreateToken(populateExp: true);
+                Log.Information($"Authentication succeeded for user {user.UserName}");
+                return Ok(tokenDto);
             }
-            var tokenDto = await _service.AuthenticationService.CreateToken(populateExp: true);
-            Log.Information($"Authentication succeeded for user {user.UserName}");
-            return Ok(tokenDto);
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Login failed for user {UserName}", user.UserName);
+                return StatusCode(500, ex.Message); // temporary - remove before final deploy
+            }
         }
     }
 }
